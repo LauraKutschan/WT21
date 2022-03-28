167dbb58b066bd9034664c670ce1ea036ed7fcd2
Laura Kutschan <laurakutschan@Lauras-MacBook-Pro.local>
2022-03-28 14:43:58 +0200

    final

--- Projekt/Backend/routes/routesPlan.js
+++ Projekt/Backend/routes/routesPlan.js
@@ -2,14 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Plan = require('../models/plan');
 
-// eine GET-Anfrage
-router.get('/', async(req, res) => {
-
-    res.send({ message: "Hello FIW!" });
-});
-
-
-
 // get all Plans
 router.get('/yourPlants/plan', async(req, res) => {
     const allPlans = await Plan.find();
@@ -20,7 +12,6 @@ router.get('/yourPlants/plan', async(req, res) => {
 // post one plan
 router.post('/yourPlants/:id/plan', async(req, res) => {
     const newPlan = new Plan({
-        plant: req.body.plant,
         date: req.body.date,
         activity: req.body.activity,
         idPlant: req.body.idPlant,
@@ -43,7 +34,6 @@ router.get('/yourPlants/plan/:id', async(req, res) => {
     }
 })
 
-
 // get all plans via idPlant
 router.get('/yourPlants/:id/plan', async(req, res) => {
     try {
@@ -58,39 +48,53 @@ router.get('/yourPlants/:id/plan', async(req, res) => {
     }
 })
 
-// update one plan
-router.patch('/yourPlants/plan/:id', async(req, res) => {
+// get all plans via idPlant and entry
+router.get('/yourPlants/:id/plan/:activity', async(req, res) => {
     try {
-        const plan = await Plan.findOne({ _id: req.params.id })
-
-        if (req.body.plant) {
-            plan.plant = req.body.plant
-        }
-
-        if (req.body.date) {
-            plan.date = req.body.date
-        }
-
-        if (req.body.activity) {
-            plan.activity = req.body.activity
-        }
-
-        await Plan.updateOne({ _id: req.params.id }, plan);
-        res.send(plan)
+        const plan = await Plan.find({ idPlant: req.params.id, activity: req.params.activity });
+        console.log(req.params);
+        res.send(plan);
     } catch {
-        res.status(404)
-        res.send({ error: "Plan does not exist!" })
+        res.status(404);
+        res.send({
+            error: "Plans does not exist!"
+        });
     }
-});
+})
+
 
 // delete one plan via id
-router.delete('/yourPlants/plan/:id', async(req, res) => {
+router.delete('/yourPlants/:id/plan/:id', async(req, res) => {
     try {
         await Plan.deleteOne({ _id: req.params.id })
         res.status(204).send()
     } catch {
         res.status(404)
         res.send({ error: "Plan does not exist!" })
+    }
+});
+
+
+// delete all Plans via activity
+router.delete('/yourPlants/:id/:activity', async(req, res) => {
+    try {
+        await Plan.deleteMany({ idPlant: req.params.id, activity: req.params.activity });
+        res.status(204).send()
+    } catch {
+        console.log(req.params.activity);
+        res.status(404)
+        res.send({ error: req.params.id })
+    }
+});
+
+// delete all Plans via card
+router.delete('/yourPlants/:id/plan', async(req, res) => {
+    try {
+        await Plan.deleteMany({ idPlant: req.params.id });
+        res.status(204).send()
+    } catch {
+        res.status(404)
+        res.send({ error: req.params.id })
     }
 });
 
--- Projekt/frontend/src/app/app.module.ts
+++ Projekt/frontend/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './components/register/register.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
@@ -20,22 +19,22 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import {NavComponent} from "./nav/nav.component";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { PlanterComponent } from './planter/planter.component';
-import { InfoComponent } from './info/info.component';
-import { DetailedComponent } from './detailed/detailed.component';
-import { CardComponent } from './detailed/card/card.component';
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatNativeDateModule} from "@angular/material/core";
-import { EditDialogComponent } from './planter/edit-dialog/edit-dialog.component';
-import {MatDialogModule} from "@angular/material/dialog";
-import { AlreadyExistsDialogComponent } from './register/already-exists-dialog/already-exists-dialog.component';
-import { RegistrationSucceededComponent } from './register/registration-succeeded/registration-succeeded.component';
-import { PopularPlantsComponent } from './popular-plants/popular-plants.component';
-import {TokenInterceptorService} from "./shared/token-interceptor.service";
-import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { NavComponent } from "./components/nav/nav.component";
+import { HttpClientModule } from "@angular/common/http";
+import { PlanterComponent } from './components/planter/planter.component';
+import { CardComponent } from './components/card/card.component';
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { EditDialogComponent } from './components/planter/edit-dialog/edit-dialog.component';
+import { MatDialogModule } from "@angular/material/dialog";
+import { AlreadyExistsDialogComponent } from './components/register/already-exists-dialog/already-exists-dialog.component';
+import { RegistrationSucceededComponent } from './components/register/registration-succeeded/registration-succeeded.component';
+import { PopularPlantsComponent } from './components/home/popular-plants/popular-plants.component';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { DeleteDialogComponent } from './components/planter/delete-dialog/delete-dialog.component';
+import { AddDialogComponent } from './components/planter/add-dialog/add-dialog.component';
+import { DeleteEntriesDialogComponent } from './components/planter/delete-entries-dialog/delete-entries-dialog.component';
 
 @NgModule({
   declarations: [
@@ -45,13 +44,14 @@ import {IvyCarouselModule} from 'angular-responsive-carousel';
     HomeComponent,
     NavComponent,
     PlanterComponent,
-    InfoComponent,
-    DetailedComponent,
     CardComponent,
     EditDialogComponent,
     AlreadyExistsDialogComponent,
     RegistrationSucceededComponent,
     PopularPlantsComponent,
+    DeleteDialogComponent,
+    AddDialogComponent,
+    DeleteEntriesDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -78,13 +78,7 @@ import {IvyCarouselModule} from 'angular-responsive-carousel';
     MatDialogModule,
     IvyCarouselModule
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptorService,
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
